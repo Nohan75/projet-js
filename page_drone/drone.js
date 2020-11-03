4b0165fefcb9caaa883afc44871fbf69e3ed44f5
Walid <walidhaddoury@outlook.fr>
2020-11-03 10:47:57 +0100

    debut du js drone

---
+++ page_drone/drone.js
@@ -0,0 +1,13 @@
+switch {
+    case: 'descriptionDJI'
+     // code block
+     break;
+    case: 'description'
+     // code block
+     break;
+    case: 'description'
+     // case block
+     break;
+    default:
+         break;
+}
