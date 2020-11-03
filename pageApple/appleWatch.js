bdc26705603db0a3d44fc3bc5fabe939c866a74d
Nohan75 <nohanmarielouise@gmail.com>
2020-11-03 15:03:29 +0100

    Changement d'image au click js

--- pageApple/appleWatch.js
+++ pageApple/appleWatch.js
@@ -27,3 +27,46 @@ function showCard() {
 }
 showCard();
 
+function activeButton() {
+    for(let i = 0; i < awButton.length; i++){
+        awButton[i].addEventListener("click", () => {
+            switch (i){
+                case 0:
+                    awButton[i].classList.toggle('isActive');
+                    break;
+                case 1:
+                    awButton[i].classList.toggle('isActive');
+                    break;
+                case 2:
+                    awButton[i].classList.toggle('isActive');
+                    break;
+            }
+        });
+    }
+}
+activeButton();
+
+const colorButton = document.getElementsByClassName('color');
+let selectImg = document.querySelector('img.watch-img');
+console.log(selectImg);
+function changeImg() {
+    for(let j = 0; j < colorButton.length; j++){
+        colorButton[j].addEventListener("click", () => {
+            switch (j){
+                case 0:
+                    selectImg.setAttribute('src', 'blackWatch.jpeg');
+                    console.log('BLACK');
+                    break;
+                case 1:
+                    selectImg.setAttribute('src', 'whiteWatch.jpeg');
+                    console.log('WHITE');
+                    break;
+                case 2:
+                    selectImg.setAttribute('src', 'redWatch.jpeg');
+                    console.log('RED');
+                    break;
+            }
+        });
+    }
+}
+changeImg();
