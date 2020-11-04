f0cfc29ed44ad0eba5b2509e0c9af15a2b700d19
Nohan75 <nohanmarielouise@gmail.com>
2020-11-04 16:04:00 +0100

    Test cache et navigation

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-prompt("COMMENT ÇA VA ?");
-document.getElementById("demo").innerHTML ="Oui";
+const myInput = document.querySelector('input');
+const okButton = document.querySelector('button');
+
+okButton.addEventListener('click', save);
+
+function save(){
+    let cookie = myInput.value;
+    localStorage.setItem('search', cookie);
+
+    switch (cookie){
+        case 'apple watch':
+            window.location.href = 'pageApple/appleWatch.html';
+            break;
+        case 'drone':
+            window.location.href = 'page_drone/drone.html';
+            break;
+        case 'phone':
+            window.location.href = 'phone/phone.html';
+            break;
+    }
+}
